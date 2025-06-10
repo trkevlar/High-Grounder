9791f9a2fde89a4992769dcba42a7ff82e2269c1
Renaldi <ren252829@gmail.com>
2025-06-10 12:58:46 +0800

    finish beta version

--- Scripts/enemyScripts/boss_skeleton.gd
+++ Scripts/enemyScripts/boss_skeleton.gd
@@ -1,13 +1,9 @@
 extends CharacterBody2D
 
-class_name bossGolem
+class_name bossSkeleton
 
-@export var projectile_scene: PackedScene
-@export var ranged_attack_range: float = 150.0
-var use_projectile: bool = false
-
-@export var detect_range = 200.0  # Increased detection range for boss
-@export var attack_range = 20.0   # Attack range
+@export var detect_range = 150.0  # Increased detection range for boss
+@export var attack_range = 40.0   # Attack range
 var owner_spawner: Node = null
 
 const speed = 30
@@ -31,7 +27,7 @@ var target_player: CharacterBody2D
 var playerInArea = false
 
 var takingDamageTimer: float = 0.0
-var takingDamageDuration: float = 0.2
+var takingDamageDuration: float = 0.8
 var attackCooldownTimer: float = 0.0
 var attackCooldown: float = 1.5
 
@@ -50,21 +46,17 @@ func _ready():
 	$AnimatedSprite2D.connect("frame_changed", Callable(self, "_on_frame_changed"))
 
 func _on_frame_changed():
-	if $AnimatedSprite2D.animation == "attack" and $AnimatedSprite2D.frame == 2:
+	if $AnimatedSprite2D.animation == "idle" and $AnimatedSprite2D.frame == 1:
 		if isDealingDamage and not has_dealt_damage:
 			deal_damage_to_player()
-			has_dealt_damage = true
-	elif $AnimatedSprite2D.animation == "attackRange" and $AnimatedSprite2D.frame == 8:
-		if isDealingDamage and not has_dealt_damage:
-			shoot_projectile()
 			has_dealt_damage = true
 	else:
 		has_dealt_damage = false
 		
 func _on_animation_finished():
-	if $AnimatedSprite2D.animation in ["attack", "attackRange"]:
+	if $AnimatedSprite2D.animation == "idle":
 		isDealingDamage = false
-		stop_attacking()   # ← ini meng-reset is_attacking & can_attack
+		stop_attacking()
 			
 func deal_damage_to_player():
 	if playerInArea and not dead and not takingDamage and target_player and is_instance_valid(target_player):
@@ -86,12 +78,10 @@ func _process(delta):
 			if playerInArea and not is_attacking and can_attack and not dead:
 				start_attacking()
 	
-	if attackCooldownTimer > 0.0:
+	if attackCooldownTimer > 0:
 		attackCooldownTimer -= delta
-		if attackCooldownTimer <= 0.0:
-			can_attack = true      # cooldown selesai
 	else:
-		can_attack = true 
+		can_attack = true
 
 	# Improved player detection logic like enemyHuman
 	if Global.playerAlive:
@@ -99,42 +89,21 @@ func _process(delta):
 		if is_instance_valid(target_player):
 			var to_player = target_player.global_position - global_position
 			var distance = global_position.distance_to(target_player.global_position)
-			dir.x = sign(to_player.x)
-			if is_attacking and distance > detect_range:
-				stop_attacking()  
-			# 1. Prioritaskan serangan melee jika dalam area dekat
-			elif distance < attack_range:
-				if is_attacking and use_projectile and not takingDamage:
-					stop_attacking()
-					attackCooldownTimer = 0.0      
-				use_projectile = false
-				if can_attack and not is_attacking and not takingDamage:
-					velocity.x = 0
-					start_attacking()
-				#return
 
-			# 2. RANGED (kalau tidak di jarak melee)
-			elif distance < ranged_attack_range:
-				use_projectile = true
-				if can_attack and not is_attacking and not takingDamage:
-					velocity.x = 0
-					start_attacking()
-				#return
-
-			# 3. Jika masih dalam jarak deteksi, kejar
-			elif distance <= detect_range:
+			if distance <= detect_range:
 				isEnemyChase = true
-				# Cegah gerakan saat sedang menyerang dengan projectile
-				if not (is_attacking and use_projectile):
+				if distance > attack_range or not playerInArea or takingDamage:
+					dir.x = sign(to_player.x)
 					velocity.x = speed * dir.x
+					isDealingDamage = false
 				else:
 					velocity.x = 0
-
-			# 4. Di luar jarak deteksi, diam
+					if can_attack and not isDealingDamage and not is_attacking:
+						start_attacking()
 			else:
 				isEnemyChase = false
 				velocity.x = 0
-
+				dir.x = 0
 	else:
 		isEnemyChase = false
 		velocity.x = 0
@@ -144,24 +113,20 @@ func _process(delta):
 	handleAnimation()
 	move_and_slide()
 
-func move(_delta):
-	if dead:
-		velocity.x = 0
-		return
-
-	if use_projectile:
-		velocity.x = 0
-		return
-		
-	if takingDamage:
-		var knockbackDir = position.direction_to(target_player.position) * knockbackForce
-		velocity.x = knockbackDir.x
-		return
-
-	if isEnemyChase and target_player:
-		velocity.x = sign(target_player.position.x - position.x) * speed
+func move(delta):
+	if !dead:
+		if !isEnemyChase:
+			velocity += dir * speed * delta
+		elif isEnemyChase and !takingDamage and target_player:
+			var dirToPlayer = position.direction_to(target_player.position) * speed
+			velocity.x = dirToPlayer.x
+			if velocity.x != 0:
+				dir.x = sign(velocity.x)
+		elif takingDamage:
+			var knockbackDir = position.direction_to(target_player.position) * knockbackForce
+			velocity.x = knockbackDir.x
 	else:
-		velocity.x = dir.x * speed
+		velocity.x = 0
 
 func handleAnimation():
 	var animatedSprite = $AnimatedSprite2D
@@ -175,11 +140,7 @@ func handleAnimation():
 		animatedSprite.play("hit")
 	# Commented out attack animation
 	elif isDealingDamage:
-		if $AnimatedSprite2D.animation == "attack":
-			animatedSprite.play("attack")
-		if $AnimatedSprite2D.animation == "attackRange":
-			animatedSprite.play("attackRange")
-			
+		animatedSprite.play("idle")
 	elif isEnemyChase and abs(velocity.x) > 0.1:
 		animatedSprite.play("run")
 	else:
@@ -187,14 +148,14 @@ func handleAnimation():
 		velocity.x = 0  
 
 	if dir.x < 0:
-		animatedSprite.flip_h = true
-	elif dir.x > 0:
 		animatedSprite.flip_h = false
+	elif dir.x > 0:
+		animatedSprite.flip_h = true
 
 func drop_item():
 	if not has_dropped_item:
 		has_dropped_item = true
-		var item_scene = preload("res://Scenes/item/healing_item.tscn")
+		var item_scene = preload("res://Scenes/item/item_life_steal.tscn")
 		var dropped_item = item_scene.instantiate()
 		get_parent().call_deferred("add_child", dropped_item)
 		dropped_item.global_position = global_position
@@ -235,38 +196,45 @@ func start_attacking():
 	has_dealt_damage = false
 	if dead or takingDamage or is_attacking:
 		return
-
+		
 	is_attacking = true
 	can_attack = false
 	isDealingDamage = true
 	attackCooldownTimer = attackCooldown
-
-	if use_projectile:
-		$AnimatedSprite2D.play("attackRange")
-	else:
-		$AnimatedSprite2D.play("attack")
-
-func shoot_projectile():
-	if use_projectile:
-		if projectile_scene == null or target_player == null:
-			return
-		var projectile = projectile_scene.instantiate()
-		get_parent().add_child(projectile)
-		
-		# Hitung arah ke player
-		var dir_to_player = (target_player.global_position - global_position).normalized()
-		
-		# Tempatkan projectile dan atur rotasi
-		projectile.global_position = global_position + Vector2(dir.x * 24, -8)
-		projectile.direction = dir_to_player
-		projectile.rotation = dir_to_player.angle()  # <-- atur rotasi berdasarkan arah
-
+	# Commented out attack animation
+	#$AnimatedSprite2D.play("attack")
+	$AnimatedSprite2D.play("idle")  # Use idle animation instead
 
 func stop_attacking():
 	is_attacking = false
 	isDealingDamage = false
-	#can_attack = true
-	use_projectile = false
+	can_attack = true
+		
+
+func _on_bos_slime_hitbox_area_entered(area: Area2D) -> void:
+	if area == Global.playerDamageZone:
+		takeDamage(Global.playerDamageAmount)
+		
+		
+
+
+func _on_bos_slime_deal_damage_area_entered(area: Area2D) -> void:
+	if area == Global.playerhitBox and not dead:
+		playerInArea = true
+		if not is_attacking and not takingDamage:
+			start_attacking()
+
+
+func _on_bos_slime_deal_damage_area_exited(area: Area2D) -> void:
+	if area == Global.playerhitBox:
+		playerInArea = false
+		stop_attacking()
+		if is_instance_valid(target_player):
+			var distance = global_position.distance_to(target_player.global_position)
+			if distance > detect_range:
+				isEnemyChase = false
+				velocity.x = 0
+				dir.x = 0
 
 const FLOOR_MASK: int = 1 << 0  # Ganti sesuai collision layer TileMap kamu
 
@@ -298,36 +266,3 @@ func spawn_enemies() -> void:
 				enemy.global_position = ground_pos
 				get_parent().add_child(enemy)
 				spawned += 1
-
-
-func _on_bos_golem_hitbox_area_entered(area: Area2D) -> void:
-	if area == Global.playerDamageZone:
-		takeDamage(Global.playerDamageAmount)
-
-
-func _on_bos_golem_deal_damage_area_entered(area: Area2D) -> void:
-	if area == Global.playerhitBox and not dead:
-		playerInArea = true
-
-		# paksa ganti ke melee kalau sedang menembak
-		if is_attacking and use_projectile and not takingDamage:
-			stop_attacking()
-			attackCooldownTimer = 0.0   # boleh langsung melee
-			use_projectile = false
-
-		if not is_attacking and not takingDamage:
-			start_attacking()
-
-
-func _on_bos_golem_deal_damage_area_exited(area: Area2D) -> void:
-	if area == Global.playerhitBox:
-		playerInArea = false
-		if use_projectile:  # Reset flag agar bisa berpindah logika serangan
-			use_projectile = false
-		stop_attacking()
-		if is_instance_valid(target_player):
-			var distance = global_position.distance_to(target_player.global_position)
-			if distance > detect_range:
-				isEnemyChase = false
-				velocity.x = 0
-				dir.x = 0
